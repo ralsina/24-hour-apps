3f9bd6c32a8b844ea73f79c278e3a51f90a06169
Roberto Alsina <ralsina@netmanagers.com.ar>
2009-10-27 10:22:33 -0300

    Handbook building structure

--- dieschere/manual-sources/runme.sh
+++ dieschere/manual-sources/runme.sh
@@ -1,3 +1,8 @@
-#!/bin/sh
-rst2qhc handbook.txt howto.txt -o . --namespace dieschere --filterattributes dieschere --rst2htmlopts="--stylesheet=style.css --link-stylesheet" --manifest MANIFEST --create-qhcp
+#!/bin/bash
+mkdir out 2>/dev/null
+rst2qhc handbook.txt howto.txt -o out --namespace dieschere --filterattributes dieschere --rst2htmlopts="--stylesheet=style.css --link-stylesheet" --manifest MANIFEST --create-qhcp
+pushd out
 qcollectiongenerator project.qhcp -o collection.qhc
+assistant -collectionFile collection.qhc -register doc.qhc
+popd
+cp out/collection.qhc ../manual
